36e05d3c6b5d18866b8ca157f3198bc8856c1d4e
Evgeny <evgenygorbushin@yandex.ru>
2023-03-18 00:20:42 +0300

    Добавлен вызов метода addNewNumberStepsPerDay()

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] Args) {
         Scanner scanner = new Scanner(System.in);
-        StepTracker inserter = new StepTracker();
+        StepTracker inserter = new StepTracker(scanner);
         while (true) {
             printMenu();
             int usersInput = scanner.nextInt();
