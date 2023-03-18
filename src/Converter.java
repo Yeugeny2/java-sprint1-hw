5d22a3ab3481742e7fafcd19c12cba46892056c0
Evgeny <evgenygorbushin@yandex.ru>
2023-03-18 22:10:43 +0300

    Дошел до шага 11

--- src/Converter.java
+++ src/Converter.java
@@ -1,3 +1,18 @@
 public class Converter {
 
+    int convertToKm(int steps) {
+      double lenghtOfStep = 0.00075;                                                           //int lenghtOfStep = 1;
+      double distanse =(lenghtOfStep * steps)/1000;                                            //int distance = (lenghtOfStep * steps)/1000;
+                                                                                               //return distance;
+        return steps;
+    }
+
+
+    int convertStepsToKilocalories(int steps) {
+        int oneStep = 50; // за один шаг сжигается 50 калорий
+        int lostCalories = (oneStep * steps)/1000; // умножаем количество сжигаемых калорий за один шаг на
+                                                    // количество пройденных шагов и переводим в килоКалории
+        return lostCalories;
+    }
+
 }
